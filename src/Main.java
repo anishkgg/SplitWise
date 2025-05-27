62b08bbc8f78d4e59067ecb57bcf26f1eeaac165
Anish Raj <anishrajkgg@gmail.com>
2025-05-28 00:38:00 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import controller.SplitWiseController;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        SplitWiseController splitWiseController = new SplitWiseController();
+        splitWiseController.startProgram();
+    }
+}
