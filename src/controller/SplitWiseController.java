af7ec033790d4f27aa97d00b84d653d81b28b601
Anish Raj <anishrajkgg@gmail.com>
2026-08-12 22:08:45 +0530

    fix in ssh

--- src/controller/SplitWiseController.java
+++ src/controller/SplitWiseController.java
@@ -13,7 +13,8 @@ public class SplitWiseController {
     UserService userService;
     TransactionService transactionService;
     GroupService groupService;
-    public SplitWiseController(){
+
+    public SplitWiseController() {
         this.scn = new Scanner(System.in);
         this.groupService = new GroupService();
         this.transactionService = new TransactionService();
@@ -21,8 +22,8 @@ public class SplitWiseController {
     }
 
 
-    public void startProgram(){
-        while(true){
+    public void startProgram() {
+        while (true) {
             System.out.println("Hey What you want to do ? Select below options");
             System.out.println(
                     "1. create-group\n" +
@@ -30,17 +31,17 @@ public class SplitWiseController {
                             "3. create user\n" +
                             "4. Show which users owes how much amount in the group and whom he owes");
             int opt = scn.nextInt();
-            if(opt == 1){
+            if (opt == 1) {
                 System.out.println("User selected opt 1 i.e. group");
                 groupService.createGroup(scn);
                 System.out.println("Group created successfully");
-            }else if(opt == 2){
+            } else if (opt == 2) {
 
-            }else if(opt == 3){
+            } else if (opt == 3) {
 
-            }else if(opt == 4){
+            } else if (opt == 4) {
 
-            }else{
+            } else {
                 System.out.println("Wrong option entered. Enter value again");
                 continue;
             }
